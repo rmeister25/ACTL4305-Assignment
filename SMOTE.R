781e9441b68b97962ff318355007ad0b279221c3
eatcatsanddogs <140568967+eatcatsanddogs@users.noreply.github.com>
2025-10-23 17:34:30 +1100

    update smote datasets

--- SMOTE.R
+++ SMOTE.R
@@ -6,18 +6,37 @@ library(glmnet)
 library(smotefamily)
 library(themis)
 
-setwd("Desktop/ACTL4305-Assignment")
+options(scipen = 999)
+# setwd("Desktop/ACTL4305-Assignment")
 freely_data <- read.csv("Cleaned_Destinations_Dates_Freely_Data.csv")
 
+# Removing NA values ~ 8000 data points lost
 freely_data <- freely_data %>%
-  filter(!is.na(trip_start_date_clean),    # remove rows that have no travel dates
-         !is.na(trip_end_date_clean)) %>%
-  dummy_cols(
-    select_columns = c("discount", "platform", "traveller_categories"),
-    remove_selected_columns = TRUE
-  )
+  mutate(across(
+    everything(),
+    ~ {
+      x <- trimws(as.character(.x))                  # remove whitespace & convert factors to character
+      ifelse(x %in% c("#N/A", "#DIV/0!", "NA", ""),  # list all “fake” NA values
+             NA, 
+             x)
+    }
+  )) %>%
+  filter(!is.na(trip_start_date_clean),
+         !is.na(trip_end_date_clean),
+         !is.na(destinations),
+         !is.na(log_ppp),
+         !is.na(Human.rights.index),
+         !is.na(water_death_rate))    
 
-#Separating Boosts
+# Separating Regions
+regions <- unique(unlist(strsplit(freely_data$regions, ",\\s*")))
+regions
+
+for (reg in regions) {
+  freely_data[[reg]] <- as.integer(grepl(reg, freely_data$regions))
+}
+
+# Separating Boosts
 boost_cols <- paste0("boost_", 1:8, "_name")  # boost_1_name ... boost_8_name
 boosts <- unique(unlist(freely_data[boost_cols]))
 boosts <- boosts[!is.na(boosts)]  # remove NAs
@@ -45,6 +64,13 @@ for (j in 1:8) {
   }
 }
 
+# ONE HOT ENCODING
+freely_data <- freely_data %>%
+  dummy_cols(
+    select_columns = c("discount", "platform", "traveller_categories"),
+    remove_selected_columns = TRUE
+  )
+
 quote_data <- freely_data %>%
   mutate(
     trip_start_date_clean = dmy(trip_start_date_clean),
@@ -56,6 +82,9 @@ quote_data <- freely_data %>%
     family = traveller_categories_Family,
     group = traveller_categories_Group,
     others = traveller_categories_Other,
+    human_rights_index = Human.rights.index,
+    rule_of_law_index = Rule.of.Law.Index,
+    extra_cancellation = as.numeric(extra_cancellation),
     extra_cancellation = if_else(is.na(extra_cancellation), 0, extra_cancellation),
     extra_cancellation_amount = extra_cancellation,
     convert = case_when(
@@ -90,12 +119,33 @@ quote_data <- freely_data %>%
          "Motorcycle Cover",
          "extra_cancellation_amount",
          "carer_load_ratio",
+         "Asia",
+         "Europe",
+         "Oceania",
+         "Middle East",
+         "North America",
+         "Africa",
+         "South America",
+         "Central America",
+         "Caribbean",
+         "Worldwide",
+         #"Antarctica",
+         "log_ppp",
+         "average_ppp",
+         "human_rights_index",
+         "rule_of_law_index",
+         "water_death_rate",
+         "avg_crime_rate",
+         "num_people",
          "convert")
 
 # Partitioning Data to 80% for training and 20% for testing
 set.seed(185)
+
 quote_data <- quote_data %>%
   mutate(across(everything(), as.numeric))
+
+
 inTrain <- createDataPartition(
   y = quote_data$convert,
   p = .8,
@@ -110,25 +160,40 @@ train_r <- as.factor(train$convert)
 test_r <- as.factor(test$convert)
 
 # Scaling road data
-mean_train <- colMeans(train[,-28])
-sd_train <- apply(train[,-28], 2, sd)
+mean_train <- colMeans(train[, non_binary_cols])
+sd_train <- apply(train[, non_binary_cols], 2, sd)
 
-train_scaled <- cbind(scale(train[,1:27]), convert = train[,28])
+
+## Scaling
+binary_cols <- which(sapply(quote_data, function(x) all(unique(x) %in% c(0, 1))))
+non_binary_cols <- setdiff(1:ncol(train), binary_cols)
+
+train_scaled <- cbind(
+  scale(train[, non_binary_cols]),
+  train[, binary_cols]
+)
+
 test_scaled <- as.matrix(
-  cbind(scale(test[,1:27], center = mean_train, scale = sd_train),
-        convert = test[,28]))
+  cbind(scale(test[, non_binary_cols], center = mean_train, scale = sd_train),
+        test[, binary_cols]))
 
 
 # Balancing Imbalanced Dataset
 # SMOTE to oversample minority class
-# x <- as.data.frame(train_scaled[,-28])
-# y <- as.factor(train_scaled[,28])
-
-x <- as.data.frame(train[,-28])
-y <- as.factor(train[,28])
-genData = SMOTE(x,y, K = 5, dup_size = 5)
+x_scaled <- as.data.frame(train_scaled[,-28])
+y_scaled <- as.factor(train_scaled[,28])
+genData = SMOTE(x_scaled,y_scaled, K = 5, dup_size = 5)
 genData <- genData$data
 table(genData$class)
-
 write.csv(genData, "training_SMOTE_dataset.csv")
-write.csv(test, "test_dataset.csv")
+write.csv(test_scaled, "test_dataset.csv")
+
+# ## NON-SCALED training data
+# x <- as.data.frame(train[,-28])
+# y <- as.factor(train[,28])
+# genData = SMOTE(x,y, K = 5, dup_size = 5)
+# genData <- genData$data
+# table(genData$class)
+# 
+# write.csv(genData, "training_scaled_SMOTE_dataset.csv")
+# write.csv(test_scaled, "test_scaled_dataset.csv")
